0c3e9e294d14f34c3548a6c87bffeda0091372f1
gregorian <gregorian.zhang@gmail.com>
2013-12-31 02:15:36 +0800

    循环读取字典运行命令

--- checksys.py
+++ checksys.py
@@ -20,7 +20,8 @@ commandlist={
     "w":["w"],
     "last":["last","-n","10"],
     #当前登入了系统,最后登入系统的日志信息
-    "history":["history","20"],
+    "history":["tail","~/.bash_history"],
+    #"history":["history","20"],
     #查看当前系统最后20条命令
     "ps":["ps","aux"],
     "pstree":["pstree","-a"],
@@ -28,33 +29,38 @@ commandlist={
     "netstat":["netstat","-nuxtlp"],
     #当前系统监听的服务
     "free":["free","-m"],
-    "top":["top","-n","1"],
+    "top":["top","-bn","1"],
     #显示cpu和mem资源
     "lspci":["lspci"],
     "dmidecode":["dmidecode"],
     #硬件相关信息
-    "iostat":["iostat","-kx","2"],
+    "iostat":["iostat","-kx","2","10"],
     "vmstat":["vmstat","2","10"],
     "mpstat":["mpstat","2","10"],
-    "dstat":["dstat","--top-io","--top-bio"],
+    "dstat":["dstat","--top-io","--top-bio","1","10"],
     #磁盘IO性能
     "mount":["mount"],
     "df":["df","-h"],
     #挂载点和文件系统
     "sysctl":["sysctl","-a"],
     "interrupts":["cat","/proc/interrupts"],
-    "ip_conntrack":["cat","/proc/net/ip_conntrack"],
-    "ss":["ss","-s"]
+    "ss":["ss","-s"],
     #kernel参数，中断
-
+    "env":["env"]
     }
-b=commandlist['w']
 #print b
 #print datetime.now()
 #Flog.write(str(datetime.now()))
 def command(c):
-    Flog.write(str(datetime.now())+"\n")
+    Flog.flush()
+    Flog.write(str(datetime.now())+"     "+str(c)+"\n")
     Flog.flush()
     call(c,stdout=Flog)
+    Flog.flush()
 
-command(b)
+for k,v in commandlist.items():
+    print k
+    b=commandlist[k]
+    print b
+    command(b)
+
